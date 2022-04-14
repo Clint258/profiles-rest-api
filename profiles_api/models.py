eabaee47ba9ff9403c8e9130df78c9b7a9262d30
Clint Pereira <clintlucasfernando@gmail.com>
2022-04-14 12:39:06 +0200

    Ativei user Admin no Django

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -13,18 +13,21 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('Users must have an email address')
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
+
         user.set_password(password)
         user.save(using=self._db)
 
-        return users
+        return user
 
-        def create_superuser(self, email, name, password):
-            """Create and save a new superuser with given details"""
-            user = self.create_user(email, name, password)
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-            user.is_superuser = True
-            user.is_staff = True
-            user.save(using=self._db)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
@@ -34,6 +37,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
+
     USERNAME_FIELD='email'
     REQUIRED_FIELDS =['name']
 
@@ -41,7 +45,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """retrieve full name of user"""
         return self.name
 
-        def get_short_name(self):
+    def get_short_name(self):
             """retrieve short name of user"""
             return self.name
 
